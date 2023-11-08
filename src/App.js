e05592d746b9e1440e3ec9d3c6b483fffaa83209
natigGit <moothman@inbox.ru>
2023-11-08 14:49:33 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-undef */
+import { sql } from "@vercel/postgres";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+if (process.env.VERCEL_ENV === "production") {
+  connectionString = process.env.POSTGRES_URL = process.env.PROD_POSTGRES_URL;
+} else if (process.env.VERCEL_ENV === "preview") {
+  connectionString = process.env.POSTGRES_URL = process.env.STAGING_POSTGRES_URL;
+} else if (process.env.VERCEL_ENV === "development") {
+  connectionString = process.env.POSTGRES_URL = process.env.DEV_POSTGRES_URL;
 }
 
-export default App;
+function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const { rows } = await sql`SELECT * from pizzas`;
+        setData(rows)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+  return (
+    <div>
+      natig's page
+      {data ? data.map((row) => (
+        <div key={row.id}>
+          {row.name} - {row.price}
+        </div>
+      ))
+        :
+        <div>Nothing found</div>
+      }
+    </div>
+  )
+}
+
+export default App
