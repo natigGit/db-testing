3b03d875c9133a10f7d63d628bdb12f95fd8fef9
natigGit <moothman@inbox.ru>
2023-11-08 15:01:56 +0400

    fixed App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import { sql } from "@vercel/postgres";
 import { useEffect, useState } from "react";
 
-if (process.env.VERCEL_ENV === "production") {
-  connectionString = process.env.POSTGRES_URL = process.env.PROD_POSTGRES_URL;
-} else if (process.env.VERCEL_ENV === "preview") {
-  connectionString = process.env.POSTGRES_URL = process.env.STAGING_POSTGRES_URL;
-} else if (process.env.VERCEL_ENV === "development") {
-  connectionString = process.env.POSTGRES_URL = process.env.DEV_POSTGRES_URL;
-}
+// if (process.env.VERCEL_ENV === "production") {
+//   connectionString = process.env.POSTGRES_URL = process.env.PROD_POSTGRES_URL;
+// } else if (process.env.VERCEL_ENV === "preview") {
+//   connectionString = process.env.POSTGRES_URL = process.env.STAGING_POSTGRES_URL;
+// } else if (process.env.VERCEL_ENV === "development") {
+//   connectionString = process.env.POSTGRES_URL = process.env.DEV_POSTGRES_URL;
+// }
 
 function App() {
 
